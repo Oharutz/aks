f280740e945ba83372af363fbe640fbd4a22254e
oharutz <oharutz@gmail.com>
2024-12-02 17:44:58 +0200

    Tried to force values in the Terraform Apply section

--- variables.tf
+++ variables.tf
@@ -1,11 +1,15 @@
 variable "location" {
+  default = "West US"
 }
 
 variable "resource_group_name" {
+  default = "basicAksRg"
 }
 
 variable "aks_cluster_name" {
+  default = "basicAKS"
 }
 
 variable "subscription_id" {
+  default = ""
 }
