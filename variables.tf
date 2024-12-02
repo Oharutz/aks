fe4dc7413127328af453f25deddcaa14f44b0c1f
oharutz <oharutz@gmail.com>
2024-12-02 17:27:46 +0200

    Tried to reset the values for the defaults in the variables.tf file

--- variables.tf
+++ variables.tf
@@ -1,15 +1,11 @@
 variable "location" {
-  default = "West US"
 }
 
 variable "resource_group_name" {
-  default = "basicAksRg"
 }
 
 variable "aks_cluster_name" {
-  default = "basicAKS"
 }
 
 variable "subscription_id" {
-  default = ""
 }
