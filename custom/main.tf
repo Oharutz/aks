2d23ba112bc5b071b99fca02d2028a668c6676c3
oharutz <oharutz@gmail.com>
2024-12-03 14:23:02 +0200

    Fixed inputs

--- custom/main.tf
+++ custom/main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "main" {
 
 # Create a Virtual Network
 resource "azurerm_virtual_network" "main" {
-  name                = "${var.resource_group_name}-vnet"
+  name                = "${var.kubernetes_cluster_name}-vnet"
   location            = azurerm_resource_group.main.location
   resource_group_name = azurerm_resource_group.main.name
   address_space       = ["10.0.0.0/16"]
@@ -18,7 +18,7 @@ resource "azurerm_virtual_network" "main" {
 
 # Create a Subnet
 resource "azurerm_subnet" "main" {
-  name                 = "${var.resource_group_name}-subnet"
+  name                 = "${var.kubernetes_cluster_name}-subnet"
   resource_group_name  = azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
   address_prefixes     = ["10.0.1.0/24"]
--- privateAksWithACR/main.tf
+++ privateAksWithACR/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_subnet" "main" {
 
 # Create an Azure Container Registry (ACR)
 resource "azurerm_container_registry" "main" {
-  name                = "${var.resource_group_name}acr"
+  name                = "${var.kubernetes_cluster_name}acr"
   resource_group_name = azurerm_resource_group.main.name
   location            = azurerm_resource_group.main.location
   sku                 = "Basic"
