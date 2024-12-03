161f9712408113c877173694c1da289513783ad6
oharutz <oharutz@gmail.com>
2024-12-03 16:53:33 +0200

    Added Private DNS Objects

--- privateAksWithACR/main.tf
+++ privateAksWithACR/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   resource_group_name = azurerm_resource_group.main.name
   dns_prefix          = "${var.kubernetes_cluster_name}-dns"
   private_cluster_enabled = false
-  
+
   default_node_pool {
     name       = "default"
     node_count = 1
@@ -58,4 +58,87 @@ resource "azurerm_role_assignment" "acr_pull" {
   scope                = azurerm_container_registry.main.id
   role_definition_name = "AcrPull"
   principal_id         = azurerm_kubernetes_cluster.main.identity[0].principal_id
+}
+
+# Assign ACR Pull Role to AKS System Identity
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.main.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_kubernetes_cluster.main.identity[0].principal_id
+}
+
+# Private DNS Zone for AKS
+resource "azurerm_private_dns_zone" "aks_dns" {
+  name                = "privatelink.${var.location}.azmk8s.io"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Private DNS Zone for ACR
+resource "azurerm_private_dns_zone" "acr_dns" {
+  name                = "privatelink.azurecr.io"
+  resource_group_name = azurerm_resource_group.main.name
+}
+
+# Virtual Network Link for AKS DNS
+resource "azurerm_private_dns_zone_virtual_network_link" "aks_vnet_link" {
+  name                  = "aks-vnet-link"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.aks_dns.name
+  virtual_network_id    = azurerm_virtual_network.main.id
+}
+
+# Virtual Network Link for ACR DNS
+resource "azurerm_private_dns_zone_virtual_network_link" "acr_vnet_link" {
+  name                  = "acr-vnet-link"
+  resource_group_name   = azurerm_resource_group.main.name
+  private_dns_zone_name = azurerm_private_dns_zone.acr_dns.name
+  virtual_network_id    = azurerm_virtual_network.main.id
+}
+
+# Private Endpoint for AKS API Server
+resource "azurerm_private_endpoint" "aks_endpoint" {
+  name                = "aks-private-endpoint"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_service_connection {
+    name                           = "aks-connection"
+    private_connection_resource_id = azurerm_kubernetes_cluster.main.id
+    subresource_names              = ["management"]
+    is_manual_connection           = false
+  }
+}
+
+# Private Endpoint for ACR
+resource "azurerm_private_endpoint" "acr_endpoint" {
+  name                = "acr-private-endpoint"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  subnet_id           = azurerm_subnet.main.id
+
+  private_service_connection {
+    name                           = "acr-connection"
+    private_connection_resource_id = azurerm_container_registry.main.id
+    subresource_names              = ["registry"]
+    is_manual_connection           = false
+  }
+}
+
+# DNS Record for AKS in Private Zone
+resource "azurerm_private_dns_a_record" "aks_dns_record" {
+  name                = "aks-api"
+  zone_name           = azurerm_private_dns_zone.aks_dns.name
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.aks_endpoint.private_ip_address]
+}
+
+# DNS Record for ACR in Private Zone
+resource "azurerm_private_dns_a_record" "acr_dns_record" {
+  name                = azurerm_container_registry.main.name
+  zone_name           = azurerm_private_dns_zone.acr_dns.name
+  resource_group_name = azurerm_resource_group.main.name
+  ttl                 = 300
+  records             = [azurerm_private_endpoint.acr_endpoint.private_ip_address]
 }
