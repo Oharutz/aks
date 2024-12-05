08a321f19d360882045d8f094d627b023c10c24e
oharutz <oharutz@gmail.com>
2024-12-05 19:51:59 +0200

    Fixing ACR name

--- privateAksAcrJumpbox/modules/acr/main.tf
+++ privateAksAcrJumpbox/modules/acr/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "main" {
-  name                = ""${var.prefix}-${random_string.unique.result}acr""
+  name                = "${var.prefix}${random_string.unique.result}acr"
   resource_group_name = var.resource_group_name
   location            = var.location
   sku                 = "Premium"
