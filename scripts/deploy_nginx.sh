294a321ad7caafaff73d9b5333d0e28e94552ef7
oharutz <oharutz@gmail.com>
2024-12-07 18:08:56 +0200

    Added bash script

---
+++ scripts/deploy_nginx.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+set -x
+
+echo "Authenticating with Azure using Managed Identity..."
+az login --identity
+
+echo "Fetching AKS credentials for the cluster..."
+az aks get-credentials --resource-group $1 --name $2 --overwrite-existing
+
+echo "Adding Helm repository..."
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+echo "Deploying the NGINX ingress controller..."
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+  --namespace ingress-basic --create-namespace \
+  --set controller.service.annotations."service.beta.kubernetes.io/azure-load-balancer-internal"="true"
+
+echo "Validating the deployment..."
+kubectl get pods -n ingress-basic
+kubectl get svc -n ingress-basic
+
+echo "NGINX ingress controller deployment completed successfully."
